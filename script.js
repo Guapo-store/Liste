b700b95a65d7f9a9400ec49bdde5e0d9d5d3c0be
DELON <86734716+Guapo-store@users.noreply.github.com>
2023-01-02 22:32:55 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -35,4 +35,32 @@ btns.forEach((btn,i) =>{
   btn.addEventListener("click",()=>{
     sliderNavv(i);
   })
-})
+});
+
+
+var repeat = function(activeClass){
+  let active = document.getElementsByClassName("active");
+  let i = 1;
+
+  var repeater = () =>{
+    setTimeout(function(){
+      [...active].forEach((activeSlide)=>{
+        activeSlide.classList.remove("active");
+      });
+      slides[i].classList.add("active");
+      btns[i].classList.add("active");
+      rows[i].classList.add("active");
+      i++
+      if(slides.length == i){
+        i = 0;
+      }
+      if(i >= slides.length){
+        return;
+      }
+      repeater()
+    },3000)
+    repeater()
+  }
+  repeat()
+}
+
